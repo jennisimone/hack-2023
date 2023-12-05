c40efd47081d6c56ec33d83a00cc93abf58d3738
Sam Harrison <sam.harrison@autotrader.co.uk>
2023-12-05 11:53:52 +0000

    chore: add tile size and prettier formatting

--- src/index.js
+++ src/index.js
@@ -1,53 +1,47 @@
-import * as Phaser from 'phaser';
-
+import * as Phaser from "phaser";
 
 const sceneConfig = {
-    active: false,
-    visible: false,
-    key: "Game"
-}
+  active: false,
+  visible: false,
+  key: "Game",
+};
 
 const CANVAS_WIDTH = 960;
-const CANVAS_HEIGHT = 640
-class GameScene extends Phaser.Scene
-{
-    constructor ()
-    {
-        super(sceneConfig);
-    }
+const CANVAS_HEIGHT = 640;
+export class GameScene extends Phaser.Scene {
+  constructor() {
+    super(sceneConfig);
+  }
 
-    preload ()
-    {
-        this.load.image("interior-tiles", "assets/office-interior.png");
-        this.load.tilemapTiledJSON("background", "assets/floor-four.json");
-    }
-      
-    create ()
-    {
-        this.map = this.make.tilemap({key: "background"});
-        const tileset = this.map.addTilesetImage("Background", "interior-tiles");
-        this.layer = this.map.createLayer("Tile Layer 1", tileset, 0, 0);
-    }
+  static TILE_SIZE = 48;
 
-    update ()
-    {
+  preload() {
+    this.load.image("interior-tiles", "assets/office-interior.png");
+    this.load.tilemapTiledJSON("background", "assets/floor-four.json");
+  }
 
-    }
+  create() {
+    this.map = this.make.tilemap({ key: "background" });
+    const tileset = this.map.addTilesetImage("Background", "interior-tiles");
+    this.layer = this.map.createLayer("Tile Layer 1", tileset, 0, 0);
+  }
+
+  update() {}
 }
 
 const gameConfig = {
-    title: "NomNom Dash",
-    type: Phaser.AUTO,
-    render: {
-        antialias: false
-    },
-    parent: 'game',
-    scale: {
-        width: CANVAS_WIDTH,
-        height: CANVAS_HEIGHT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
-    scene: GameScene
+  title: "NomNom Dash",
+  type: Phaser.AUTO,
+  render: {
+    antialias: false,
+  },
+  parent: "game",
+  scale: {
+    width: CANVAS_WIDTH,
+    height: CANVAS_HEIGHT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  scene: GameScene,
 };
 
-const game = new Phaser.Game(gameConfig);
+export const game = new Phaser.Game(gameConfig);
