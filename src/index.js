8ec34a1a5b302e75f4a6ccc3ed3ead2c004b0639
Jenni Foster <jenni.foster@autotrader.co.uk>
2023-12-05 14:06:25 +0000

    Setup alerts and room code

--- src/index.js
+++ src/index.js
@@ -18,6 +18,30 @@ class MyGame extends Phaser.Scene
         this.map = this.make.tilemap({key: "background"});
         const tileset = this.map.addTilesetImage("Background", "interior-tiles");
         this.layer = this.map.createLayer("Tile Layer 1", tileset, 0, 0);
+        const rooms = this.map.getObjectLayer("room").objects;
+        this.alertTime(rooms)
+        console.log({rooms})
+    }
+
+
+    alertTime(rooms) {
+        let randomRoom = rooms[Math.floor(Math.random() * rooms.length)];
+
+        const alertText = this.add.text(480,20,'').setOrigin(0.5);
+
+        const timer = this.add.timeline([{
+            at: 500,                // ms
+            run: () => {
+                alertText.setText(`Food alert! Sandwiches outside ${randomRoom.name}`)
+            },
+        },{
+            at: 10000,                // ms
+            run: () => {
+                alertText.setText('')
+            },
+        }]);
+
+        timer.play()
     }
 }
 
