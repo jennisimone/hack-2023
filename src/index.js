563e363e71ed08646868dbad523fb3dbf6c7235d
Jenni Foster <jenni.foster@autotrader.co.uk>
2023-12-05 15:22:48 +0000

    Add food to food alerts!

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@ class MyGame extends Phaser.Scene
     {
         this.load.image("interior-tiles", "assets/office-interior.png");
         this.load.tilemapTiledJSON("background", "assets/floor-four.json");
+        this.load.spritesheet("Donuts", "assets/donuts.png", {frameHeight: 32, frameWidth: 32})
+        this.load.spritesheet("Kebabs", "assets/kebabs.png", {frameHeight: 32, frameWidth: 32})
+        this.load.spritesheet("Pizza", "assets/pizza.png", {frameHeight: 32, frameWidth: 32})
     }
       
     create ()
@@ -20,19 +23,21 @@ class MyGame extends Phaser.Scene
         this.layer = this.map.createLayer("Tile Layer 1", tileset, 0, 0);
         const rooms = this.map.getObjectLayer("room").objects;
         this.alertTime(rooms)
-        console.log({rooms})
     }
 
 
     alertTime(rooms) {
         let randomRoom = rooms[Math.floor(Math.random() * rooms.length)];
+        const foods = ["Donuts", "Kebabs", "Pizza"]
 
         const alertText = this.add.text(480,20,'').setOrigin(0.5);
+        let randomFood = foods[Math.floor(Math.random() * foods.length)];
 
         const timer = this.add.timeline([{
             at: 500,                // ms
             run: () => {
-                alertText.setText(`Food alert! Sandwiches outside ${randomRoom.name}`)
+                alertText.setText(`Food alert! ${randomFood} outside ${randomRoom.name}`)
+                this.add.sprite(randomRoom.x, randomRoom.y, randomFood)
             },
         },{
             at: 10000,                // ms
