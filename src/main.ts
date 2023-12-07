c66afe6ac1c00378fe7e24cce7894244bcb2ee44
Jenni Foster <jenni.foster@autotrader.co.uk>
2023-12-07 11:19:11 +0000

    Add stair functionality!!

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import TiledObject = Phaser.Types.Tilemaps.TiledObject
 import TilemapLayer = Phaser.Tilemaps.TilemapLayer
 import Home from './home'
 import Win from './win'
+import Sprite = Phaser.GameObjects.Sprite
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
     active: false,
@@ -16,6 +17,8 @@ const CANVAS_HEIGHT = 960;
 
 export class GameScene extends Phaser.Scene {
     private alertText: Phaser.GameObjects.Text
+    private playerTwoCamera: Phaser.Cameras.Scene2D.Camera
+
     constructor() {
         super(sceneConfig);
     }
@@ -44,19 +47,19 @@ export class GameScene extends Phaser.Scene {
     }
 
     public create() {
-        const floorLayer = this.createTilemaps()
+        const {floorLayer, stairs} = this.createTilemaps()
 
-        this.physics.world.setBounds(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        this.physics.world.setBounds(0, 0, CANVAS_WIDTH * 3, CANVAS_HEIGHT);
 
         this.setupPlayerOne()
         this.setupPlayerTwo()
         this.setupPhysics(floorLayer)
         this.setupAudio()
+        this.setupStairs(stairs)
         this.alertTime()
     }
 
     public update(_time: number, delta: number) {
-
         if (this.player != undefined) {
             if (this.input.keyboard.addKey('A').isDown) {
                 this.player.moveLeft();
@@ -109,10 +112,10 @@ export class GameScene extends Phaser.Scene {
             texture: "playerTwo"
         });
         this.playerTwo.init()
-        let playerTwoCamera = this.cameras.add((CANVAS_WIDTH / 2) + 1, 0, CANVAS_WIDTH / 2, CANVAS_HEIGHT)
-        playerTwoCamera.setBounds(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-        playerTwoCamera.startFollow(this.playerTwo);
-        playerTwoCamera.roundPixels = true;
+        this.playerTwoCamera = this.cameras.add((CANVAS_WIDTH / 2) + 1, 0, CANVAS_WIDTH / 2, CANVAS_HEIGHT)
+        this.playerTwoCamera.setBounds(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+        this.playerTwoCamera.startFollow(this.playerTwo);
+        this.playerTwoCamera.roundPixels = true;
     }
 
     private setupPhysics(floorLayer: Phaser.Tilemaps.TilemapLayer) {
@@ -160,7 +163,7 @@ export class GameScene extends Phaser.Scene {
 
         this.playEatingSfx()
 
-        if(this.player1Score + this.player2Score === 15) {
+        if (this.player1Score + this.player2Score === 15) {
             this.scene.start("win", {player1Score: this.player1Score, player2Score: this.player2Score})
         }
 
@@ -173,7 +176,10 @@ export class GameScene extends Phaser.Scene {
         const randomRoom = this.rooms[Math.floor(Math.random() * this.rooms.length)];
         const foods = ["Donuts", "Kebabs", "Pizza"];
 
-        this.alertText = this.add.text(240, 20, "", {color: "black", backgroundColor: "white"}).setOrigin(0.5).setScrollFactor(0);
+        this.alertText = this.add.text(240, 20, "", {
+            color: "black",
+            backgroundColor: "white"
+        }).setOrigin(0.5).setScrollFactor(0);
         const randomFood = foods[Math.floor(Math.random() * foods.length)];
 
         const timer = this.add.timeline([
@@ -206,30 +212,39 @@ export class GameScene extends Phaser.Scene {
     }
 
     private createTilemaps() {
-        const map = this.make.tilemap({key: "floor4"});
+        const map = this.make.tilemap({key: "allFloors"});
         const interiorTileset = map.addTilesetImage("OfficeInterior", "interior-tiles");
         const objectTileset = map.addTilesetImage("office-props", "interior-props");
+        const upArrowTileset = map.addTilesetImage("UpArrow", "up-arrow");
+        const downArrowTileset = map.addTilesetImage("DownArrow", "down-arrow");
+
         const floorLayer = map.createLayer("Floor", interiorTileset, 0, 0);
         const wallLayer = map.createLayer("Walls", interiorTileset, 0, 0);
         const propsLayer = map.createLayer("Props", objectTileset, 0, 0);
+        map.createLayer("Arrows", [upArrowTileset, downArrowTileset], 0, 0);
+
         this.rooms = map.getObjectLayer("Room").objects;
         const stairs = map.getObjectLayer("Stairs").objects;
 
         wallLayer.setCollisionByExclusion([-1], true)
         propsLayer.setCollisionByExclusion([-1], true)
         this.collidingLayers.push(wallLayer, propsLayer)
-        return floorLayer
+        return {floorLayer, stairs}
     }
 
     private loadTilemaps() {
         this.load.tilemapTiledJSON("floor4", "assets/floor4.json");
         this.load.tilemapTiledJSON("floor5", "assets/floor5.json");
         this.load.tilemapTiledJSON("floor6", "assets/floor6.json");
+        this.load.tilemapTiledJSON("allFloors", "assets/allfloors.json");
     }
 
     private loadImages() {
         this.load.image("interior-tiles", "assets/office-interior.png");
         this.load.image("interior-props", "assets/office-objects.png");
+        this.load.image("up-arrow", "assets/UpArrow.png");
+        this.load.image("down-arrow", "assets/downArrow.png");
+
         this.load.spritesheet("Donuts", "assets/donuts.png", {frameHeight: 32, frameWidth: 32})
         this.load.spritesheet("Kebabs", "assets/kebabs.png", {frameHeight: 32, frameWidth: 32})
         this.load.spritesheet("Pizza", "assets/pizza.png", {frameHeight: 32, frameWidth: 32})
@@ -246,6 +261,67 @@ export class GameScene extends Phaser.Scene {
     private loadAudio() {
         this.load.audio("music", "assets/Hack-2023.wav")
         this.load.audio('eating', 'assets/eating.mp3');
+    }
+
+    private setupStairs(stairs: Phaser.Types.Tilemaps.TiledObject[]) {
+        const stairSprites: Sprite[] = []
+        stairs.forEach((stair, index) => {
+            stairSprites.push(new Sprite(this, stair.x, stair.y, null))
+            this.physics.world.enableBody(stairSprites[index])
+            if (stair.name.startsWith("Up")) {
+                this.physics.add.overlap(this.player, stairSprites[index], () => this.moveUp("1", stair.name), null, this)
+                this.physics.add.overlap(this.playerTwo, stairSprites[index], () => this.moveUp("2", stair.name), null, this)
+            }
+            if (stair.name.startsWith("Down")) {
+                this.physics.add.overlap(this.player, stairSprites[index], () => this.moveDown("1", stair.name), null, this)
+                this.physics.add.overlap(this.playerTwo, stairSprites[index], () => this.moveDown("2", stair.name), null, this)
+            }
+        })
+
+    }
+
+    private moveUp(player: string, name: string) {
+        let newX = 0
+        if (name === "Up5") {
+            newX = 1350
+        }
+        if (name === "Up6") {
+            newX = 2039
+        }
+        if (player === "1"){
+            this.cameras.main.setBounds(this.cameras.main.getBounds().x + CANVAS_WIDTH, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+            this.cameras.main.startFollow(this.player)
+            this.player.x = newX
+        }
+        if (player === "2"){
+            this.playerTwoCamera.setBounds(this.playerTwoCamera.getBounds().x + CANVAS_WIDTH, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+            this.cameras.main.startFollow(this.playerTwo)
+            this.playerTwo.x = newX
+        }
+    }a
+
+    private moveDown(player: string, name: string) {
+        let newX = 0
+        let newY = 0
+        if (name === "Down4") {
+            newX = 594
+            newY = 939
+        }
+        if (name === "Down5") {
+            newX = 1719
+            newY = 840
+        }
+
+        if (player === "1"){
+            this.cameras.main.setBounds(this.cameras.main.getBounds().x - CANVAS_WIDTH, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+            this.player.x = newX
+            this.player.y = newY
+        }
+        if (player === "2"){
+            this.playerTwoCamera.setBounds(this.playerTwoCamera.getBounds().x - CANVAS_WIDTH, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+            this.playerTwo.x = newX
+            this.playerTwo.y = newY
+        }
     }
 }
 
